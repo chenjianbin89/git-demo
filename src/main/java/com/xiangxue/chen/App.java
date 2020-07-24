dac119e89b464144faff3b23f334ec2005d165f1
chen <1030033085@qq.com>
2020-07-24 12:33:42 +0800

    version01

---
+++ src/main/java/com/xiangxue/chen/App.java
@@ -0,0 +1,13 @@
+package com.xiangxue.chen;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
