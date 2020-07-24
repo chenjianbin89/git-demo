7d51374d8378e7e93423da4e0829f8cf404f3f08
chen <1030033085@qq.com>
2020-07-24 13:47:38 +0800

    try again

---
+++ src/main/java/com/xiangxue/chen/Apple.java
@@ -0,0 +1,4 @@
+package com.xiangxue.chen;
+
+public class Apple {
+}
---
+++ src/main/java/com/xiangxue/chen/GitDemoApplication.java
@@ -0,0 +1,18 @@
+package com.xiangxue.chen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GitDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GitDemoApplication.class, args);
+    }
+    @RequestMapping("/")
+    public String helloGit(){
+        return "Hello Git!----->master version1";
+    }
+}
