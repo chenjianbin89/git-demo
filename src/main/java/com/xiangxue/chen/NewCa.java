757db5b4e442eb34c2a1a615a66cb2cac132b002
lenovo <1030033085@qq.com>
2021-04-11 20:27:18 +0800

    新增class

---
+++ src/main/java/com/xiangxue/chen/NewCa.java
@@ -0,0 +1,14 @@
+package com.xiangxue.chen;
+
+/**
+ * @author lenovo
+ * @title: NewCa
+ * @projectName git-demo
+ * @description: TODO
+ * @date 2021年04月11日 20:25
+ */
+public class NewCa {
+    public static void main(String[] args) {
+        System.out.println("nihao");
+    }
+}
