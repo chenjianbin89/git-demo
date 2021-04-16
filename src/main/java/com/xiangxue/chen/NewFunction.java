ac87fb283c441084f877ee798fbb75cbc96bb108
chenjianbin89 <1030033085@qq.com>
2021-04-16 14:14:43 +0800

    提交新功能

---
+++ src/main/java/com/xiangxue/chen/NewFunction.java
@@ -0,0 +1,12 @@
+package com.xiangxue.chen;
+
+/**
+ * @author jitwxs
+ * @date 2021年04月16日 14:11
+ */
+public class NewFunction {
+
+    public static void main(String[] args) {
+        System.out.println("this is a new function!");
+    }
+}
