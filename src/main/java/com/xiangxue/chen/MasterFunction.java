da33d8ab26b22562d97911af7a5c41ffd0d3e47b
chenjianbin89 <1030033085@qq.com>
2021-04-16 14:17:52 +0800

    master提交新功能

---
+++ src/main/java/com/xiangxue/chen/MasterFunction.java
@@ -0,0 +1,11 @@
+package com.xiangxue.chen;
+
+/**
+ * @author jitwxs
+ * @date 2021年04月16日 14:17
+ */
+public class MasterFunction {
+    public static void main(String[] args) {
+        System.out.println("this is master function");
+    }
+}
