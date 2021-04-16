3b2512bfeb9618a9ab822c422b2fe603931cb3eb
chenjianbin89 <1030033085@qq.com>
2021-04-16 14:27:48 +0800

    LeadingFunction

---
+++ src/main/java/com/leading/gitdemo/LeadingFunction.java
@@ -0,0 +1,13 @@
+package com.leading.gitdemo;
+
+import java.sql.SQLOutput;
+
+/**
+ * @author jitwxs
+ * @date 2021年04月16日 14:27
+ */
+public class LeadingFunction {
+    public static void main(String[] args) {
+        System.out.println("leading function!");
+    }
+}
